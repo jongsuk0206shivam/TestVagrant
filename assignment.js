b30050b703640aa1e7cbea6e9355f4123bd6b8b0
jongsuk0206shivam <52959157+jongsuk0206shivam@users.noreply.github.com>
2022-06-11 00:43:41 +0530

    Update assignment.js

--- assignment.js
+++ assignment.js
@@ -3,6 +3,12 @@ let teams_perf = [
     { name: 'LSG', points: 18, last5: ['won', 'lose', 'lose', 'win', 'win'] },
     { name: 'RR', points: 16, last5: ['won', 'lose', 'won', 'lose', 'lose'] },
     { name: 'DC', points: 14, last5: ['won', 'won', 'lose', 'won', 'lose'] },
+    { name: 'RCB', points: 14, last5: ['lose', 'won', 'won', 'lose', 'lose'] },
+    { name: 'KKR', points: 12, last5: ['lose', 'won', 'won', 'lose', 'won'] },
+    { name: 'PBKS', points: 12, last5: ['lose', 'won', 'lose', 'won', 'lose'] },
+    { name: 'SRH', points: 12, last5: ['won', 'lose', 'lose', 'lose', 'lose'] },
+    { name: 'CSK', points: 8, last5: ['lose', 'lose', 'won', 'lose', 'won'] },
+    { name: 'MI', points: 6, last5: ['lose', 'won', 'lose', 'won', 'won'] }
 ]
 
 
@@ -11,7 +17,7 @@ let teams_perf = [
 function retrieveTeamsWithTwoConsLoss(){
     let filtered_teams = teams_perf.filter((team)=>{
         let cons_count = 0;
-        for(let i = 0; i < 5; i++){
+        for(let i = 0; i < 10; i++){
           if(team.last5[i] === 'lose'){
             cons_count++;
           }else{
@@ -36,7 +42,7 @@ function retrieveTeamsWithTwoConsLoss(){
 function retrieveTeamsWithNConsLoss(N){
     let filtered_teams = teams_perf.filter((team)=>{
         let cons_count = 0;
-        for(let i = 0; i < 5; i++){
+        for(let i = 0; i < 10; i++){
           if(team.last5[i] === 'lose'){
             cons_count++;
           }else{
